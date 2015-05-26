e77536286defc0e85e5b3d448ac96edfe0fd6cab
Michael Sypolt <msypolt@transitguru.limited>
2015-05-26 15:17:21 -0400

    Some bootstrapping and config making.

---
+++ affirm_api.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @file
+ * Bootstrap file for Affirm API Connector
+ *
+ * This includes all the necessary 
+ * to access the router, which will then access appropriate controllers
+ *
+ * @category   Bootstrap
+ * @package    Serverphu
+ * @author     Michael Sypolt <msypolt@transitguru.limited>
+ * @copyright  Copyright (c) 2015
+ * @license    http://www.gnu.org/licenses/gpl-3.0.txt
+ * @version    0.5.0 
+ */
+//
+define('DOC_ROOT', __DIR__);
+
+// Load the settings
+require_once (DOC_ROOT . '/config.php');
+
+// Load the classes
+$PATH = DOC_ROOT . '/classes';
+$includes = scandir($PATH);
+foreach ($includes as $include){
+  if (is_file($PATH . '/' . $include) && $include != '.' && $include != '..' && fnmatch("*.php", $include)){
+    require_once ($PATH . '/' . $include);
+  }
+}
---
+++ classes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file
+ * AffirmAPI Class
+ * 
+ * Handles user input and provides output
+ *
+ * @category Request Handling
+ * @package Affirm API
+ * @author Michael Sypolt <msypolt@nurelm.com>
+ * @copyright Copyright (c) 2015
+ * @license http://www.gnu.org/licenses/gpl-3.0.txt
+ * @version Release: @package_version@
+ *
+ */
+class AffirmAPI {
+  //TODO: actually get this thing built
+}
